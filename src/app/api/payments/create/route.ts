73e5ec601f4766ab10a906101652e7f433a0f827
fabregasCool <108883197+fabregasCool@users.noreply.github.com>
2026-09-23 17:32:58 +0000

    paiement jeko9

--- src/app/api/payments/create/route.ts
+++ src/app/api/payments/create/route.ts
@@ -66,11 +66,13 @@ export async function POST(req: NextRequest) {
     const amountCents = Math.round(montant * 100);
     console.log("💰 Paiement à créer:", { amountCents, montant });
 
+    const amount = Math.round(montant); // XOF n'a pas de sous-unité , c'est les frais de Jeko(1%)
+
     const reference = `ORDONNANCE-${resourceId}-${Date.now()}`;
 
     if (provider === "jeko") {
       const jekoRes = await createJekoPaymentRequest({
-        amountCents,
+        amountCents: amount,
         reference,
         paymentMethod,
       });
